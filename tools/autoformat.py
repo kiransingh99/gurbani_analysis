258114a9ba2e9a5fedaec9ecece2ee8ff2d7c6da
kiransingh99 <65014905+kiransingh99@users.noreply.github.com>
2022-08-30 22:49:02 +0100

    Add pylint tests before merging (#27)
    Before merging a PR into main, it is now required to have passing pylint checks.
    
    Other code changes in this PR are to get pylint passing.

--- tools/autoformat.py
+++ tools/autoformat.py
@@ -1,3 +1,14 @@
+# ------------------------------------------------------------------------------
+# autoformat.py - Script to check for correctly formatted code
+#
+# August 2022, Gurkiran Singh
+#
+# Copyright (c) 2022
+# All rights reserved.
+# ------------------------------------------------------------------------------
+
+"""Checks for badly formatted code and optionally reformats it."""
+
 import argparse
 import enum
 import subprocess
@@ -12,7 +23,31 @@ class _BlackReturnCodes(enum.IntEnum):
     FILE_DOES_NOT_EXIST = 2
 
 
-def _run_cmd(args):
+def _handle_cmd_error(return_code, cmd):
+    """
+    Handles the exception raised due to a failure occurring when running the
+    black CLI. If there's an unknown error code, an error message is printed to
+    log this. The script then exits with the same return code as received from
+    black.
+
+    :param exc:
+        subprocess.CalledProcessError tuple containing returncode and command
+        ran.
+    """
+
+    # Check if return code is known.
+    try:
+        _BlackReturnCodes(return_code)
+    except ValueError:
+        print(
+            "\nAn unexpected error occurred when running the command:"
+            + {" ".join(cmd)}
+        )
+    finally:
+        sys.exit(return_code)
+
+
+def _run_black(args):
     """
     Runs `black` command based on input parameters.
 
@@ -22,54 +57,36 @@ def _run_cmd(args):
     :returns:
         CompletedProcess namespace object with command ran and returncode.
     """
-    cmd = (
-        [
-            "black",
-            "--check" if args.check else None,
-            "--quiet" if args.quiet else None,
-            "--verbose" if args.verbose else None,
-            # Line length is hardcoded to be 80 chars as changing this value
-            # could reformat the whole workspace. There is no need for a user to
-            # change this value.
-            "--line-length",
-            "80",
-        ]
-        + args.files
-        + (["--exclude"] + args.exclude if args.exclude else [None])
-    )
-    cmd = list(filter(lambda x: x is not None, cmd))
+
+    cmd = ["black"]
+    if args.check:
+        cmd.append("--check")
+    if args.quiet:
+        cmd.append("--quiet")
+    if args.verbose:
+        cmd.append("--verbose")
+
+    # Line length is hardcoded to be 80 chars as changing this value
+    # could reformat the whole workspace. There is no need for a user to
+    # change this value.
+    cmd += ["--line-length", "80"]
+
+    cmd += args.files
+
+    if args.exclude:
+        cmd += ["--exclude"] + args.exclude
 
     if args.verbose:
         print(f"Running: {' '.join(cmd)}\n")
 
-    p = subprocess.run(cmd)
-
-    return p
-
-
-def _handle_return_code(process):
-    """
-    Handles the return code received when running the black CLI. If there's an
-    unexpected error code, an error message is printed to log this. Then exits
-    the script with the same return code as received from black.
-
-    :param process:
-        CompletedProcess namespace object with command ran and returncode.
-    """
-    # Check if return code is known.
     try:
-        _BlackReturnCodes(process.returncode)
-    except ValueError:
-        print(
-            "\nAn unexpected error occurred when running the command: '{}'".format(
-                " ".join(process.args)
-            )
-        )
-    finally:
-        sys.exit(process.returncode)
+        subprocess.run(cmd, check=True)
+    except subprocess.CalledProcessError as exc:
+        _handle_cmd_error(exc.returncode, exc.cmd)
 
 
 def main():
+    """Main function for autoformat CLI. Parses and handles CLI input"""
     parser = argparse.ArgumentParser(
         description="Run autoformat checks on given files. Optionally reformat the given files."
     )
@@ -103,13 +120,13 @@ def main():
         "--verbose",
         action="store_true",
         default=False,
-        help="Also emit messages to stderr about files that were not changed or were ignored due to exclusion patterns.",
+        help="Also emit messages to stderr about files that were not changed"
+        + "or were ignored due to exclusion patterns.",
     )
 
     args = parser.parse_args()
 
-    p = _run_cmd(args)
-    _handle_return_code(p)
+    _run_black(args)
 
 
 if __name__ == "__main__":
