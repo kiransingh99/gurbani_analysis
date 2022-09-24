128b627dc33b4bb951367c08ce59c1e029446d29
kiransingh99 <65014905+kiransingh99@users.noreply.github.com>
2022-09-24 21:46:04 +0100

    Improve Regex matching in copyright checks (#43)
    Make regex rules stricter and more accurate

--- tools/copyright.py
+++ tools/copyright.py
@@ -20,6 +20,7 @@ file in the repo. It should be formatted as follows:"""
 
 __all__ = []
 
+from dataclasses import dataclass
 from datetime import date
 
 import argparse
@@ -38,9 +39,16 @@ class _CopyrightCheckReturnCodes(cmn.ReturnCodes):
     CHANGES_REQUIRED = 1
 
 
+@dataclass(frozen=True)
+class _LineMatchInfo:
+    line_number: int
+    act_line: str
+    exp_line: str
+
+
 def _generate_expected(file_path):
     """
-    Generator tha outputs each line of a correctly formatted copyright notice.
+    Generator that outputs each line of a correctly formatted copyright notice.
 
     :param file_path:
         Path to the file to generate an copyright notice for.
@@ -49,42 +57,60 @@ def _generate_expected(file_path):
         Regex for a copyright notice, output line-by-line.
     """
     file_name = file_path.split("/")[-1]
+    notice_start_end = r"^# [-]{78}$"
+    blank_line = r"^#$"
+
+    # pylint: disable=line-too-long
     exp = [
-        "# -----------------------------------------------------------------------------",
-        f"# {file_name} - [^.*]",
-        "#",
-        "# (January|February|March|April|May|June|July|August|September|October|November|December)"
-        r" 20[0-9]{2}, [A-Za-z -]",
-        "#",
-        rf"# Copyright \(c\) (20[0-9]{{2}} - ){{0,1}}{date.today().year}",
-        "# All rights reserved.",
-        "# -----------------------------------------------------------------------------",
+        notice_start_end,
+        rf"^# {file_name} - .+$",
+        blank_line,
+        r"^# (January|February|March|April|May|June|July|August|September|October|November|December)"
+        r" 20[0-9]{2}, [A-Za-z -]+$",
+        blank_line,
+        rf"^# Copyright \(c\) (20[0-9]{{2}} - ){{0,1}}{date.today().year}$",
+        r"^# All rights reserved.$",
+        notice_start_end,
     ]
+    # pylint: enable=line-too-long
 
     for line in exp:
         yield line
 
 
-def _run_check(_):
+def _run_check(args):
     """
     Runs copyright checks on input parameters.
 
     :param args:
         Namespace object with args to run check on.
     """
-    include_files = cmn.get_all_code_files()
+
+    def read_line():
+        nonlocal line_number
+        line_number += 1
+        return f_read.readline().strip("\n")
 
     failed = {}
+    line_number = 0
+
+    include_files = cmn.get_all_code_files()
+
     for file in include_files:
+        line_number = 0
         if os.stat(file).st_size == 0:
             continue
         with open(file, encoding="utf-8") as f_read:
             for exp_line in _generate_expected(file):
-                line = f_read.readline().strip("\n")
+                line = read_line()
                 if line.startswith("#!"):
-                    f_read.readline().strip("\n")
+                    line = read_line()
                 if not re.match(exp_line, line):
-                    failed[file] = line
+                    failed[file] = _LineMatchInfo(
+                        line_number,
+                        act_line=line,
+                        exp_line=exp_line,
+                    )
                     break
 
     if failed:  # pylint: disable=no-else-return
@@ -92,8 +118,17 @@ def _run_check(_):
             "Copyright notice checks failed! "
             "The following files need to be fixed:"
         )
-        for file, line in failed.items():
-            print(f"   - {file}: {line}")
+
+        for file, line_info in failed.items():
+            if args.show_regex:
+                print(f"   - {file}:{line_info.line_number}:")
+                print(f"        `{line_info.act_line}` does not match")
+                print(f"        `{line_info.exp_line}`")
+            else:
+                print(
+                    f"   - {file}:{line_info.line_number}:    `{line_info.act_line}`"
+                )
+
         return _CopyrightCheckReturnCodes.CHANGES_REQUIRED
     else:
         print(
@@ -108,6 +143,15 @@ def main():
     parser = argparse.ArgumentParser(
         description="Check if all files have correctly formatted copyright checks. "
         "Optionally add/modify the notice on any files that fail the check."
+    )
+
+    parser.add_argument(
+        "-r",
+        "--show-regex",
+        action="store_true",
+        default=False,
+        help="If the check fails on a given file, provide the regex rule that "
+        "the malformed line should match.",
     )
 
     args = parser.parse_args()
