85bc07dde265a032130a7deb4ef351c056189f8f
Gurkiran Singh <kiran.jolly@gmail.com>
2023-01-08 01:00:02 +0000

    test with regex output

--- tools/copyright.py
+++ tools/copyright.py
@@ -81,6 +81,7 @@ def _generate_expected(file_path: str) -> Generator[str, None, None]:
         .strip()
         .split("\n")
     )
+    print(edit_dates)
     created_month = cmn.month_name_from_num(int(edit_dates[-1][5:7]))
     created_year = edit_dates[-1][:4]
     last_edited_year = edit_dates[0][:4]
